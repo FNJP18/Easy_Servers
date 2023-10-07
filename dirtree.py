f66d4a62514a948eac6be0128a244b4076fc55da
yamat-72k <125722903+yamat-72k@users.noreply.github.com>
2023-10-07 21:54:53 +0900

    Update dirtree.py

--- dirtree.py
+++ dirtree.py
@@ -1,4 +1,37 @@
 import os
+import threading
+import json
+import time
+from tkinter import messagebox as msg
+
+serverdir = ""
+pldirvar = ()
+plapppldirvar = ()
+
+def dirthread():
+    global servertree, plapppldirvar, pldirvar
+    while True:
+        try:
+            servertree = dirtree(serverdir)
+            temp = list(servertree["plugins"].keys())
+            temp.remove("*files")
+            temp += servertree["plugins"]["*files"]
+        except Exception as e:
+            msg.showerror(title="Directory Error", message=e)
+        try:
+            pldirvar = tuple(temp)
+        except:pass
+        try:
+            plapppldirvar = tuple(temp)
+        except:pass
+        time.sleep(1)
+
+def treethread():
+    global servertree
+    servertree = dirtree(serverdir)
+    with open("dir.json", "w", encoding="utf-8") as f:
+        json.dump(servertree, f, indent=4, ensure_ascii=False)
+    threading.Thread(target=dirthread, name="directory").start()
 
 def dirtree(root_dir):
     result = {}
